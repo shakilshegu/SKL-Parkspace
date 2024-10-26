6fe28de38156d77a1efddc03837f422c43d7e433
shakil <muhammadshakil1968@gmail.com>
2024-10-26 10:38:13 +0530

    Update Port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -14,7 +14,7 @@ async function bootstrap() {
 <h2>Looking for the graphql api?</h2>
 Go to <a href="/graphql" target="_blank">/graphql</a>.
 Or,
-You might also need to use the <a target="_blank" href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:3000/graphql&document=query users{users{ uid }}
+You might also need to use the <a target="_blank" href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:4000/graphql&document=query users{users{ uid }}
 ">Apollo explorer</a> for a greater experience.
 
       `,
